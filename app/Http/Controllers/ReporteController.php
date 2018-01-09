5160dd4bd022416e1482a38a2448d61c5a26199b
Luis-Martinez <31491201+luis1008@users.noreply.github.com>
2018-01-08 22:36:32 -0600

    correccion de reportes
    puse en cobranza solo los abonados, ya que tenia todos. y en compras igual.

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -13,14 +13,14 @@ class ReporteController extends Controller
 {
 	public function get_pedidos(){
         //dd($id);
-        $ped = Pedido::where('pe_status','=','ENTREGADO')->get();
+        $ped = Pedido::where('pe_pago_status','=','ABONADO')->get();
         
         return view('Reportes.ReporteCobranza')->with('pedidos',$ped);
     }
 
     public function get_compras(){
         //dd($id);
-        $com = Compra::all();
+        $com = Compra::where('cm_status','=','ABONADO')->get();
         
         return view('Reportes.ReporteCompras')->with('compras',$com);
     }
